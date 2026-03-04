21e9eaafaa1cc80b3d8fd4e0b73c7f5d64c01865
jasmineu-2006 <jasmine.u2502@gmail.com>
2026-03-04 19:57:44 +0530

    updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,26 +1,20 @@
 package com.example;
-
 public class App {
-
     public int add(int a, int b) {
         return a + b;
     }
-
     public int subtract(int a, int b) {
         return a - b;
     }
-
     public int multiply(int a, int b) {
         return a * b;
     }
-
     public double divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Cannot divide by zero");
         }
         return (double) a / b;
     }
-
     public static void main(String[] args) {
         App calculator = new App();
 
